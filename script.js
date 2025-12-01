632061a72f8d31cca4d90760c8d94a953fdfd065
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-01 18:52:43 +0100

    feat: aggiunto programma in js per biglietti treni con stampa su html

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ per controllare che la vostra logica sui prezzi funzioni correttamente, provate 
 100km, 10 anni => prezzo corretto:  €16.80
 100km, 70 anni => prezzo corretto: €12.60 */
 
+const userKm = parseFloat(prompt("inserisci il numero di chilometri che vuoi percorrere"));
+const userAge = parseInt(prompt("inserisci la tua età"));
+let minorPrice = 0 ;
+let oldPrice = 0 ;
+
+console.log(userKm, userAge) ;
+
+let ticketPrice = 0.21 * userKm ; 
+
+if (userAge < 18) {
+    minorPrice = ticketPrice - ticketPrice * 20 / 100;
+    console.log("prezzo biglietto minori " + "€" + minorPrice.toFixed(2));
+    document.getElementById("minor-price").innerHTML = "€" + minorPrice.toFixed(2);
+}
+
+else if (userAge >= 65) {
+    oldPrice = ticketPrice - ticketPrice * 40 / 100;
+    console.log("prezzo biglietto anziani " + "€" + oldPrice.toFixed(2));
+    document.getElementById("old-price").innerHTML = "€" + oldPrice.toFixed(2);
+}
+
+else {
+    console.log("prezzo biglietto normale " + "€" + ticketPrice.toFixed(2));
+    document.getElementById("normal-price").innerHTML = "€" + ticketPrice.toFixed(2);
+
+}
+
